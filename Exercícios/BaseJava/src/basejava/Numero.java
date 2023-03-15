2f4fc88844415a17ed2f992bf67e9af049daca20
Leonardo Macedo <macedo_22@hotmail.com>
2023-03-15 20:36:02 -0300

    aula03

--- Exercícios/BaseJava/src/basejava/Numero.java
+++ Exercícios/BaseJava/src/basejava/Numero.java
@@ -13,8 +13,12 @@ public class Numero {
     }
 
     public String parImpar (int valor){
-        return "";
-    }    
+        if(valor % 2 ==0){
+            return "PAR";
+        }else{
+            return "IMPAR";
+        }
+    }  
 
 public int maior(int v1, int v2){
 
@@ -24,6 +28,23 @@ public int maior(int v1, int v2){
         return v2;
 }
     public String ordemCrescente(int valor1, int valor2, int valor 3){
-        return "";
+        int temp = 0;
+        if(valor1 > valor2){
+            temp = valor1;
+            valor1 = valor2;
+            valor2 = temp;
+        }
+        if(valor2 > valor3){
+            temp = valor3;
+            valor3 = valor2;
+            valor2 = temp;
     }
+        if(valor1 > valor3){
+        temp = valor2;
+        valor3 = valor1;
+        valor1 = temp;
+}
+return valor1 + " - " + valor2 + " - " + valor3;
+    }
+
 }
